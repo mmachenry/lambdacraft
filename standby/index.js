57b43fdc73eff931b0a2b8569d2c76764ae7bb3e
Trevor Williams <whiltx@gmail.com>
2020-05-22 22:26:12 -0700

    Add DNS modification libraries.

--- standby/index.js
+++ standby/index.js
@@ -2,6 +2,7 @@
 
 const mc = require('minecraft-protocol');
 const Compute = require('@google-cloud/compute');
+const {DNS} = require('@google-cloud/dns');
 
 const serverVmName = 'instance-1';
 const gcpZone = 'us-central1-a';
@@ -9,6 +10,10 @@ const gcpZone = 'us-central1-a';
 const compute = new Compute();
 const zone = compute.zone(gcpZone);
 const vm = zone.vm(serverVmName);
+
+const dns = new DNS();
+const dnsZone = dns.zone('ninjavitis-zone');
+const ttl = 30;
 
 var server = mc.createServer({
     'online-mode': false,
@@ -19,15 +24,33 @@ var server = mc.createServer({
     motd: 'Lambdacraft standby server',
 });
 
+function updateDns(newIp) {
+      const newARecord = dnsZone.record('a', {
+        name: 'ninjavitis.com.',
+        data: newIp,
+        ttl: ttl
+      });
+
+      dnsZone.replaceRecords('a', newARecord).then((data) => {
+        const change = data[0];
+        const apiResponse = data[1];
+      });
+}
+
 var counter = 0;
 server.on('connection', (client) => {
     server.motd = "Lambdacraft Standby Server: " + counter++
     console.log(`Received connection number ${counter}.`);
 
-    vm.start().then((data) => {
-        const operation = data[0];
-        const apiResponse = data[1];
-        console.log(operation);
-        console.log(apiResponse);
-    });
+    vm
+        .start()
+        .then((data) => {
+            const operation = data[0];
+            const apiResponse = data[1];
+            console.log(operation);
+            console.log(apiResponse);
+        })
+        .catch(() => {
+            console.log(`something bad happened during VM startup.`)
+        });
 });
