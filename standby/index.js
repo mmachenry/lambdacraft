720b3abcd5e92a01776d07af661e5add91b7e099
Trevor Williams <whiltx@gmail.com>
2020-05-22 21:47:18 -0700

    Add GCP libraries for managing VMs for standby server.

--- standby/index.js
+++ standby/index.js
@@ -1,4 +1,14 @@
-var mc = require('minecraft-protocol');
+"use strict";
+
+const mc = require('minecraft-protocol');
+const Compute = require('@google-cloud/compute');
+
+const serverVmName = 'instance-1';
+const gcpZone = 'us-central1-a';
+
+const compute = new Compute();
+const zone = compute.zone(gcpZone);
+const vm = zone.vm(serverVmName);
 
 var server = mc.createServer({
     'online-mode': false,
@@ -10,6 +20,14 @@ var server = mc.createServer({
 });
 
 var counter = 0;
-server.on('connection', function(client) {
+server.on('connection', (client) => {
     server.motd = "Lambdacraft Standby Server: " + counter++
+    console.log(`Received connection number ${counter}.`);
+
+    vm.start().then((data) => {
+        const operation = data[0];
+        const apiResponse = data[1];
+        console.log(operation);
+        console.log(apiResponse);
+    });
 });
