66d8940b86181d2ecf0dfef3e9f22941855e8f82
Mike MacHenry <mike.machenry@gmail.com>
2022-02-25 22:37:37 -0600

    remove launch type to use default capacity provider

--- terraform/startup/lambda_handler.py
+++ terraform/startup/lambda_handler.py
@@ -7,7 +7,6 @@ def handler (event, callback):
     response = client.run_task(
         cluster = os.getenv("CLUSTER_ARN"),
         taskDefinition = os.getenv("TASK_ARN"),
-        launchType = "EC2",
         count = 1,
     )
     print(response)
