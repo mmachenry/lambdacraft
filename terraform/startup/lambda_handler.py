15b9b74e35fa0c2a2c30b704a65b733ffaa79f53
Mike MacHenry <mike.machenry@gmail.com>
2022-02-25 20:06:06 -0600

    add a log message

--- terraform/startup/lambda_handler.py
+++ terraform/startup/lambda_handler.py
@@ -2,6 +2,7 @@ import os
 import boto3
 
 def handler (event, callback):
+    print("startup lambda invoked")
     client = boto3.client("ecs")
     response = client.run_task(
         cluster = os.getenv("CLUSTER_ARN"),
