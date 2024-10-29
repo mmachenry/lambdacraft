37e2ac1e4eeb1914591b4b55c3a9efaff8a96780
Mike MacHenry <mike.machenry@gmail.com>
2024-10-28 20:27:44 -0400

    upgrade mahcine

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -12,7 +12,7 @@ variable "log_retention" {
 
 variable "game_vm_type" {
   description = "EC2 machine type on which to run the game server."
-  default     = "m5zn.large"
+  default     = "z1d.large"
   type        = string
 }
 
